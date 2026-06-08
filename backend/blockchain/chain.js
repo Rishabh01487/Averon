f438986e2df7dd624f17a45f84f323af86ead45a
Rishabh01487 <rishabhguptaa999175@gmail.com>
2026-06-08 11:45:52 +0530

    get adjustment interval

--- backend/blockchain/chain.js
+++ backend/blockchain/chain.js
@@ -6,3 +6,6 @@ const fs = require('fs');
 const path = require('path');
 const { Block } = require('./block');
 const { Transaction } = require('./transaction');
+const { MerkleTree } = require('./merkle');
+const { adjustDifficulty, validateChain, getChainStats } = require('./consensus');
+const C = require('../config/constants');
