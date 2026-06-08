8524efd2a89c79d2e1f0ff313e1308e784025bed
Rishabh01487 <rishabhgupta999175@gmail.com>
2026-06-08 11:53:12 +0530

    implement toJSON

--- backend/blockchain/block.js
+++ backend/blockchain/block.js
@@ -6,3 +6,7 @@ const crypto = require('crypto');
 const { MerkleTree } = require('./merkle');
 const { Transaction } = require('./transaction');
 const C = require('../config/constants');
+
+class Block {
+  constructor(index, previousHash, transactions = [], timestamp = Date.now()) {
+    this.version = 1;
