5a43e20ef00312a7a794ccde5e46d2f15b684754
Rishabh01487 <rishabhgupta999175@gmail.com>
2026-06-08 11:53:08 +0530

    return integrity result

--- backend/blockchain/transaction.js
+++ backend/blockchain/transaction.js
@@ -10,3 +10,7 @@ class Transaction {
   constructor(from, to, amount, type, data = {}) {
     this.from = from;               // Sender address (or 'SYSTEM' for mints)
     this.to = to;                   // Receiver address
+    this.amount = parseFloat(amount) || 0;
+    this.type = type;               // TX_TYPES constant
+    this.data = data;               // Additional payload
+    this.timestamp = Date.now();
