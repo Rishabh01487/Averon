618020a2c15242ef7c3ee1013c4676edf2eac173
Rishabh01487 <rishabhgupta999175@gmail.com>
2026-06-08 11:52:30 +0530

    set block time target

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -102,3 +102,7 @@ module.exports = {
     'Stocks & Shares',
     'Land & Real Estate',
     'Agricultural Goods',
+    'Shop Inventory',
+    'Equipment',
+    'Invoices & Bills',
+    'Vehicles',
