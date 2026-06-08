9d8afac82687da7f4c2a1d7866f8657d8a6c2633
Rishabh01487 <rishabhguptaa999175@gmail.com>
2026-06-08 11:45:35 +0530

    detect corruption

--- backend/blockchain/transaction.js
+++ backend/blockchain/transaction.js
@@ -5,3 +5,8 @@
 
 const crypto = require('crypto');
 const C = require('../config/constants');
+
+class Transaction {
+  constructor(from, to, amount, type, data = {}) {
+    this.from = from;               // Sender address (or 'SYSTEM' for mints)
+    this.to = to;                   // Receiver address
